2f6d2b0e57ae01e641507a62267c0b7161dd3617
Debesh <debesh391@gmail.com>
2023-09-24 22:51:17 +0530

    bug fixes

--- myHeaders.cpp
+++ myHeaders.cpp
@@ -326,23 +326,6 @@ void clearArr(string arr[], int size)
         arr[i]="";
     }
 }
-void clearArr(int arr[], int size)
-{
-    for(int i=0; i<size; i++)
-    {
-        arr[i]="";
-    }
-}
-void clearArr(char arr[], int size)
-{
-    for(int i=0; i<size; i++)
-    {
-        arr[i]="";
-    }
-}
-
-
-
 void printArr(int arr[], int size ) {
     cout<<"[ ";
     for(int i=0 ; i<size; i++) {
