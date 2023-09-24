b5447e1b3ba993af6bdf8439d0addb16839fc436
Debesh <debesh391@gmail.com>
2023-09-24 22:46:24 +0530

    clearArr updated at line 329

--- myHeaders.cpp
+++ myHeaders.cpp
@@ -326,6 +326,21 @@ void clearArr(string arr[], int size)
         arr[i]="";
     }
 }
+void clearArr(int arr[], int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        arr[i]="";
+    }
+}
+void clearArr(char arr[], int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        arr[i]="";
+    }
+}
+
 
 
 void printArr(int arr[], int size ) {
